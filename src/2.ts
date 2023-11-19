ebf69a5f886a16f1fbb3e0b90230e47951a6ac08
Yevhen-L <ujeen2101@gmail.com>
2023-11-19 14:25:57 +0200

    Update 2.ts

--- src/2.ts
+++ src/2.ts
@@ -14,8 +14,30 @@
 
 */
 
+// class Employee {
+//   // Заповніть модифікатори доступу
+//   name: string;
+//   department: string;
+//   salary: number;
+
+//   constructor(name: string, department: string, salary: number) {
+//     this.name = name;
+//     this.department = department;
+//     this.salary = salary;
+//   }
+
+//   getEmployeeDetails() {
+//     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
+//   }
+// }
+
+// class Manager extends Employee {
+//   // Реалізуйте конструктор та збільшіть salary на 10000
+// }
+
+// export {};
+
 class Employee {
-  // Заповніть модифікатори доступу
   name: string;
   department: string;
   salary: number;
@@ -32,8 +54,7 @@ class Employee {
 }
 
 class Manager extends Employee {
-  // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary + 10000);
+  }
 }
-
-
-export {};
